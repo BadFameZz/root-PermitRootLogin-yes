f82e121942b34ea2be107acf2c499ec6ec9e74ff
BadFameZz <81498380+BadFameZz@users.noreply.github.com>
2023-06-23 22:36:38 +0200

    Update PermitRootLogin-yes.sh

--- PermitRootLogin-yes.sh
+++ PermitRootLogin-yes.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-# Überprüfe, ob das Skript mit Root-Rechten ausgeführt wird
-if [[ $EUID -ne 0 ]]; then
-   echo "Dieses Skript muss mit Root-Rechten ausgeführt werden." 
-   exit 1
+backup_file="/etc/ssh/sshd_config.bak"
+
+# Check if backup file already exists
+if [[ -f "$backup_file" ]]; then
+    echo "Backup file already exists. Skipping backup creation."
+else
+    cp /etc/ssh/sshd_config "$backup_file"
+    echo "Backup file created."
 fi
 
-# Backup der Originaldatei erstellen
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
+# Prompt the user to choose an option
+echo "Do you want to enable or disable PermitRootLogin?"
+echo "1. Enable PermitRootLogin"
+echo "2. Disable PermitRootLogin"
+read -p "Enter your choice (1 or 2): " choice
 
-# PermitRootLogin auf "yes" setzen
-sed -i 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-# SSH-Dienst neu starten, um die Änderungen zu übernehmen
-service ssh restart
-
-echo "Die Einstellung 'PermitRootLogin' wurde auf 'yes' gesetzt."
+# Validate the user's choice and update the SSH configuration accordingly
+if [[ $choice == "1" ]]; then
+    sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+    echo "PermitRootLogin has been enabled. Restarting SSH service..."
+    service ssh restart
+    echo "SSH service restarted."
+elif [[ $choice == "2" ]]; then
+    sed -i 's/PermitRootLogin.*/#PermitRootLogin no/g' /etc/ssh/sshd_config
+    echo "PermitRootLogin has been disabled. Restarting SSH service..."
+    service ssh restart
+    echo "SSH service restarted."
+else
+    echo "Invalid choice. Exiting..."
+    exit 1
+fi
